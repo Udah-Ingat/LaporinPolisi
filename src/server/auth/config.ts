0b178cbf49f271df782b793b3c077b7e1a9fef76
andi-frame <andifarhan1094@gmail.com>
2025-07-01 18:34:37 +0800

    feat: done config for google oauth

--- src/server/auth/config.ts
+++ src/server/auth/config.ts
@@ -1,6 +1,6 @@
 import { DrizzleAdapter } from "@auth/drizzle-adapter";
 import { type DefaultSession, type NextAuthConfig } from "next-auth";
-import DiscordProvider from "next-auth/providers/discord";
+import GoogleProvider from "next-auth/providers/google";
 
 import { db } from "@/server/db";
 import {
@@ -38,16 +38,19 @@ declare module "next-auth" {
  */
 export const authConfig = {
   providers: [
-    DiscordProvider,
-    /**
-     * ...add more providers here.
-     *
-     * Most other providers require a bit more work than the Discord provider. For example, the
-     * GitHub provider requires you to add the `refresh_token_expires_in` field to the Account
-     * model. Refer to the NextAuth.js docs for the provider you want to use. Example:
-     *
-     * @see https://next-auth.js.org/providers/github
-     */
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+      authorization: {
+        params: {
+          scope: "openid email profile",
+          // Optional, to force new refresh tokens every login:
+          // prompt: "consent",
+          // access_type: "offline",
+          // response_type: "code",
+        },
+      },
+    }),
   ],
   adapter: DrizzleAdapter(db, {
     usersTable: users,
