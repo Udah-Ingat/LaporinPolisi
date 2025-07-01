908bcf1ba3ee8ea1ce5bf44d7a7680cd79c1216e
andi-frame <andifarhan1094@gmail.com>
2025-07-01 18:49:30 +0800

    feat: done login mechanism and styling

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,4 +1,7 @@
+"use client";
+
 import React from "react";
+import { signIn } from "next-auth/react";
 
 const page = () => {
   return (
@@ -6,10 +9,16 @@ const page = () => {
       <div className="text-center text-2xl font-bold">LW</div>
       <div className="bg-lapor-brown flex h-32 w-3/4 items-center justify-center rounded-xl p-5 shadow-md/40">
         <div className="bg-lapor-green flex items-center justify-center gap-5 rounded-xl px-6 py-3 shadow-md/40">
-          <button className="bg-lapor-white text-lapor-black rounded-md px-2">
+          <button
+            className="bg-lapor-white text-lapor-black rounded-md px-2 transition-colors duration-200 ease-in-out hover:bg-white hover:text-black"
+            onClick={() => signIn("google", { callbackUrl: "/" })}
+          >
             Masuk
           </button>
-          <button className="bg-lapor-black text-lapor-white rounded-md px-2">
+          <button
+            className="bg-lapor-black text-lapor-white rounded-md px-2 transition-colors duration-200 ease-in-out hover:bg-black hover:text-white"
+            onClick={() => signIn("google", { callbackUrl: "/" })}
+          >
             Daftar
           </button>
         </div>
