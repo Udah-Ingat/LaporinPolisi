96b8c4b312177f37eb2f586c78eb1e9a7c98bd05
andi-frame <andifarhan1094@gmail.com>
2025-07-01 17:20:50 +0800

    fix: tailwind v4 issue

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,11 @@
-export default {
-  plugins: {
-    "@tailwindcss/postcss": {},
+const config = {
+  content: [
+    "./src/app/**/*.{js,ts,jsx,tsx}", // For Next.js (App Router)
+  ],
+  theme: {
+    extend: {},
   },
+  plugins: ["@tailwindcss/postcss"],
 };
+
+export default config;
